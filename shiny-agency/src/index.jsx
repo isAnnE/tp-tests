94aff9a6f02e81c0592148a1292797af2e738ae8
Anne Morren <anne.morren@gmail.com>
2021-08-22 16:38:19 +0200

    archi suite

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './pages/Home'
-
+import { BrowserRouter as Router } from 'react-router-dom'
+ 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Route path="/">
+                <Home />
+            </Route>
+            <Route exact path="/survey">
+                <Survey />
+            </Route>
+        </Router>
+    </React.StrictMode>
 )
